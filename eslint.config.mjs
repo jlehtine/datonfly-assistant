79af60d201d79f62b2fbffada6a909cc1676e52e
Johannes Lehtinen <johannes.lehtinen@iki.fi>
2026-04-06 20:37:17 +0300

    Enable React code linting.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import eslint from "@eslint/js";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
@@ -33,6 +36,33 @@ export default tseslint.config(
                     varsIgnorePattern: "^_",
                 },
             ],
+        },
+    },
+    {
+        files: ["**/*.tsx"],
+        ...react.configs.flat.recommended,
+        ...react.configs.flat["jsx-runtime"],
+        languageOptions: {
+            ...react.configs.flat.recommended.languageOptions,
+            globals: {
+                ...globals.browser,
+            },
+        },
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+    },
+    {
+        files: ["**/*.tsx"],
+        ...reactHooks.configs.flat.recommended,
+    },
+    {
+        files: ["**/*.tsx"],
+        plugins: { "react-refresh": reactRefresh },
+        rules: {
+            "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
         },
     },
 );
